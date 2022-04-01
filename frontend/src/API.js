f0b4c5e3fffb9d7edeee9df37b8c7da2e113c5b4
arochey2566 <95597913+arochey2566@users.noreply.github.com>
2022-04-01 09:15:27 +0530

    Update API.js

--- frontend/src/API.js
+++ frontend/src/API.js
@@ -7,7 +7,7 @@ var baseURL;
 //    baseURL = "http://127.0.0.1:8000";
 // }
 
-baseURL = 'https://backend-hungercaptain.herokuapp.com/';
+baseURL = 'https://backend-hunger-captian.herokuapp.com/';
 
 const api = axios.create({
   baseURL: baseURL,
